b1138d66dd20bfb08742ff1ba9453fff9db62d45
Jan Górkiewicz <greencashew@users.noreply.github.com>
2020-05-27 18:08:50 +0200

    Add initial application

---
+++ src/test/java/com/trojok/find/projection/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trojok.find.projection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
