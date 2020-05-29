83d0fc987bc0062168ce705e99739f93f78e3905
Jan Górkiewicz <greencashew@users.noreply.github.com>
2020-05-29 13:47:47 +0200

    Add pyproj

---
+++ python/proj_transform.py
@@ -0,0 +1,13 @@
+import pyproj
+from pyproj import Transformer
+from pyproj import CRS
+
+transproj_eq = Transformer.from_proj(
+    proj_from='+proj=merc',
+    proj_to='+proj=airy',
+    always_xy=True,
+    skip_equivalent=True
+)
+
+for pt in transproj_eq.itransform([(20.137, 15.661)]):
+    print('{:.3f} {:.3f}'.format(*pt))
