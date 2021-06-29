e185af0320bd99b290f61d014fcd6024f10a98bb
Jan Gorkiewicz <greencashew@users.noreply.github.com>
2021-06-29 17:31:52 +0200

    Raname package

--- backend/processor.py
+++ backend/processor.py
@@ -1,21 +1,21 @@
 import numpy as np
-from backend.estimate.transform import estimation_helmert_four, optimization_polynomial_first_order, \
-    optimization_polynomial_second_order, optimization_polynomial_third_order
-from backend.estimate.transform_quality import mean_square_error
 
 from backend.crs.coordinates_transformer_async import async_convert_from_wgs84_to_crs_list
 from backend.crs.crs_list import get_applicable_crs_list_for_bounds
+from backend.estimate.estimation import estimation_polynomial_first_order, estimation_polynomial_second_order, \
+    estimation_polynomial_third_order, estimation_helmert_four
+from backend.estimate.quality import mean_square_error
 
 
 def process(input_values_map, expected_values_map, bounds):
     list_for_bounds = get_applicable_crs_list_for_bounds(bounds)
     converted_points = async_convert_from_wgs84_to_crs_list(expected_values_map, list_for_bounds)
 
-    results_polynomial_first = calculate_conversion_for(optimization_polynomial_first_order, converted_points,
+    results_polynomial_first = calculate_conversion_for(estimation_polynomial_first_order, converted_points,
                                                         input_values_map)
-    results_polynomial_second = calculate_conversion_for(optimization_polynomial_second_order, converted_points,
+    results_polynomial_second = calculate_conversion_for(estimation_polynomial_second_order, converted_points,
                                                          input_values_map)
-    results_polynomial_third = calculate_conversion_for(optimization_polynomial_third_order, converted_points,
+    results_polynomial_third = calculate_conversion_for(estimation_polynomial_third_order, converted_points,
                                                         input_values_map)
     results_helmert_four = calculate_conversion_for(estimation_helmert_four, converted_points,
                                                     input_values_map)
@@ -28,18 +28,18 @@ def process(input_values_map, expected_values_map, bounds):
     }
 
 
-def calculate_conversion_for(optimization_function, converted_points, input_values_map):
+def calculate_conversion_for(estimation_function, converted_points, input_values_map):
     results = []
     for crs in converted_points:
         items = []
         for i in range(len(input_values_map)):
             items.append([*input_values_map[i], *list(crs['converted_points'][i])])
 
-        results.append(calculate_optimization_for(optimization_function, items, crs))
+        results.append(calculate_estimation_for(estimation_function, items, crs))
     return results
 
 
-def calculate_optimization_for(function, array_of_items, crs):
+def calculate_estimation_for(function, array_of_items, crs):
     pred_x, pred_y, shift_vector_x, shift_vector_y, parameters = function(np.array(array_of_items))
     mse_overall, mse_x, mse_y = mean_square_error(shift_vector_x, shift_vector_y, len(np.array(array_of_items)))
 
