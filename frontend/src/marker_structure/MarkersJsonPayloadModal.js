a5d7f0c63e92d8c9ebf37c3635be3894245771b7
Jan Gorkiewicz <greencashew@users.noreply.github.com>
2021-08-20 14:03:10 +0200

    Add copy to clipboard button on payload view.

--- frontend/src/marker_structure/MarkersJsonPayloadModal.js
+++ frontend/src/marker_structure/MarkersJsonPayloadModal.js
@@ -1,11 +1,16 @@
 import React, {useState} from 'react';
 import {Button, Modal, ModalBody, ModalFooter, ModalHeader, NavLink} from 'reactstrap';
+import {NotificationManager} from "react-notifications";
 
 
 function MarkersJsonPayloadModal({markers}) {
     const [modal, setModal] = useState(false);
 
     const toggle = () => setModal(!modal);
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(JSON.stringify(markers, null, 2))
+        NotificationManager.success("Payload copied.", 'Success');
+    }
 
     const closeBtn = <button className="close" onClick={toggle}>&times;</button>;
 
@@ -22,6 +27,7 @@ function MarkersJsonPayloadModal({markers}) {
                     </pre>
                 </ModalBody>
                 <ModalFooter>
+                    <Button color="primary" onClick={copyToClipboard}>Copy</Button>
                     <Button color="primary" onClick={toggle}>Exit</Button>
                 </ModalFooter>
             </Modal>
